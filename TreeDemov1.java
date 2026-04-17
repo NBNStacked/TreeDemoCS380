73b1ff87b506f602ce089fdeae719f8e56013338
NBNStacked <153414505+NBNStacked@users.noreply.github.com>
2026-04-16 21:26:20 -0700

    InOrder Traversal
    Implementation of InOrder Traversal

--- TreeDemov1.java
+++ TreeDemov1.java
@@ -73,6 +73,10 @@ class Node{
 	   */
 	   public void inOrderTraversal(Node root){
 	      //implement in here
+         if(root == null){ return;}
+         inOrderTraversal(root.left);
+         System.out.print(root.value);
+         inOrderTraversal(root.right);
 	   }
 	   
 	   
